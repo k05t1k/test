84e4a0c6a31238da70fe5d173fb34d9588778747
k05t1k <isip.k.m.fionov@mpt.ru>
2024-09-10 00:44:32 +0300

    first

---
+++ Project.Tests/Tests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project;
+using System;
+
+namespace PasswordValidation.Tests
+{
+    [TestClass]
+    public class PasswordTests
+    {
+        private PasswordValidator _passwordValidator;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _passwordValidator = new PasswordValidator();
+        }
+
+        [TestMethod]
+        public void EvaluatePasswordStrength_ContainsDigit_Returns1()
+        {
+            int result = _passwordValidator.EvaluatePasswordStrength("123");
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void EvaluatePasswordStrength_ContainsLowercaseLetter_Returns2()
+        {
+            int result = _passwordValidator.EvaluatePasswordStrength("abc123");
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void EvaluatePasswordStrength_ContainsUppercaseLetter_Returns3()
+        {
+            int result = _passwordValidator.EvaluatePasswordStrength("Abc123");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void EvaluatePasswordStrength_ContainsSpecialCharacter_Returns4()
+        {
+            int result = _passwordValidator.EvaluatePasswordStrength("Abc123@#");
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void EvaluatePasswordStrength_LengthMoreThan10_Returns5()
+        {
+            int result = _passwordValidator.EvaluatePasswordStrength("A1b2c3d4e5f6!");
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void EvaluatePasswordStrength_NoCriteriaMet_Returns0()
+        {
+            int result = _passwordValidator.EvaluatePasswordStrength("");
+            Assert.AreEqual(0, result);
+        }
+
+        static void Main() 
+        {
+        
+        }
+    }
+}
---
+++ Project/Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Project
+{
+    public class PasswordValidator
+    {
+        public int EvaluatePasswordStrength(string pass)
+        {
+            if (string.IsNullOrEmpty(pass))
+                return 0;
+
+            int score = 0;
+
+            if (pass.Any(char.IsDigit))
+                score++;
+
+            if (pass.Any(char.IsLower))
+                score++;
+
+            if (pass.Any(char.IsUpper))
+                score++;
+
+            if (Regex.IsMatch(pass, @"[!@#$%^&*()_+=\[{\]};:<>|./?,-]"))
+                score++;
+
+            if (pass.Length > 10)
+                score++;
+
+            return score;
+        }
+
+        static void Main()
+        {
+            PasswordValidator validator = new PasswordValidator();
+            Console.WriteLine(validator.EvaluatePasswordStrength("ExamplePassword1!"));
+        }
+    }
+}
